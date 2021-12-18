d90d83b7ce8c7cab0ac0b8fe6b67c723d113d07c
Craig Strachan <craig@craigstrachan.com>
2021-12-18 14:47:44 +0200

    Cleanup and refactoring

--- PaceCalculatorRouteGenerator/src/PaceCalculatorRouteGenerator.java
+++ PaceCalculatorRouteGenerator/src/PaceCalculatorRouteGenerator.java
@@ -5,43 +5,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 //import SplitInputTO;
 import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
 
+public class PaceCalculatorRouteGenerator {
 
-public class TemplateGenerator {
-	  
-	public static void main(String[] args) throws JsonIOException, IOException  {
+	public static void main(String[] args) throws JsonIOException, IOException {
+
+		Logger log = Logger.getLogger("PaceCalculatorRouteGenerator");
+		log.info("*** Starting ***");
+
+		String inputFile;
+		if (args.length > 0)
+			inputFile = args[0];
+		else
+			inputFile = "/Users/craigstrachan/Downloads/input.txt";
+			log.info("Input file not specified, using default.");
+
 		ArrayList<SplitInputTO> splitInputs;
 		PaceChartTO paceChartTO = new PaceChartTO();
-		
+
 		// read input file
-		// Open the file
-		FileInputStream fstream = new FileInputStream("input.txt");
+		log.info("Reading from file:" + inputFile);
+		FileInputStream fstream = new FileInputStream(inputFile);
 		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
 
 		String[] line;
-		
+
 		/*
-		 * Sample input: 
-		 *  raceName|Ommedraai 10k, 2019"
-		 * 	plannedRaceTimeFirst|00:39:00
-		 *  plannedRaceTimeLast|01:59:00
-		 * 	plannedRaceTimeDelta|00:10:00
-		 * 	startDelay|00:01:00
-		 * 	firstFade|0
-		 * 	lastFade|2
-		 * 	distance|10.0
+		 * Sample input:
+		 raceName|Ommedraai 10k, 2019"
+		 plannedRaceTimeFirst|00:39:00
+		 plannedRaceTimeLast|01:59:00
+		 plannedRaceTimeDelta|00:10:00
+		 startDelay|00:01:00
+		 firstFade|0
+		 lastFade|2
+		 distance|10.0
 		 */
-		
+
 		line = br.readLine().split("\\|");
 		paceChartTO.setRaceName(line[1]);
-		
+
 		line = br.readLine().split("\\|");
 		paceChartTO.setPlannedRaceTimeFirst(line[1]);
-		
+
 		line = br.readLine().split("\\|");
 		paceChartTO.setPlannedRaceTimeLast(line[1]);
 
@@ -50,7 +61,7 @@ public class TemplateGenerator {
 
 		line = br.readLine().split("\\|");
 		paceChartTO.setStartDelay(line[1]);
-		
+
 		line = br.readLine().split("\\|");
 		paceChartTO.setFirstFade(Integer.parseInt(line[1]));
 
@@ -59,16 +70,17 @@ public class TemplateGenerator {
 
 		line = br.readLine().split("\\|");
 		paceChartTO.setDistance(Double.valueOf(line[1]));
-		
+
 		// setup elevations & manual weighting
 		splitInputs = new ArrayList<SplitInputTO>();
 		for (int counter = 0; counter < Math.ceil(paceChartTO.getDistance()); counter++) {
 			SplitInputTO splitInput = new SplitInputTO();
 			splitInput.setSplitNumber(counter + 1);
-			// read the elevation from the input file (remove the \t-tabs which can sneak in from pasting the input file from excel)
-			splitInput.setElevation(Integer.parseInt((br.readLine().replace("\t",""))));
+			// read the elevation from the input file (remove the \t-tabs which can sneak in
+			// from pasting the input file from excel)
+			splitInput.setElevation(Integer.parseInt((br.readLine().replace("\t", ""))));
 			splitInput.setManualWeight(100);
-			
+
 			if (counter + 1 > paceChartTO.getDistance())
 				splitInput.setSplitDistance(paceChartTO.getDistance());
 			else
@@ -78,14 +90,16 @@ public class TemplateGenerator {
 		}
 		fstream.close();
 		paceChartTO.setSplitInputs(splitInputs);
-		Gson gson =new Gson();
-		Writer writer = new FileWriter("Output/" + paceChartTO.getRaceName() + ".json");
+		Gson gson = new Gson();
+		String outputFile = inputFile + ".json";
+
+		log.info("Writing to file:" + outputFile);
+
+		Writer writer = new FileWriter(outputFile);
 		String jsonOutput = gson.toJson(paceChartTO);
 		writer.write(jsonOutput);
 		writer.close();
-		System.out.println("*** Complete ***");
-		
-        
-	    }
+		log.info("*** Complete ***");
+	}
 
 }
